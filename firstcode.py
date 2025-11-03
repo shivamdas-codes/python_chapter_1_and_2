4b4351aa9f277ca69f0d47e71c40c985e97578cf
shivam <shivam984861@gmail.com>
2025-11-03 18:43:42 +0530

    new changes

---
+++ firstcode.py
@@ -0,0 +1,109 @@
+print("hello world ","im here") 
+print("0303")
+print(33)
+# first 2 codes represent about how functions and strings are combined and give the proper output
+# third code represent that the numeric value can be written inside the "" or weitten eithout the ""
+
+print("hello world")
+# first hello world printed sucessecfully
+
+print(35+75)
+print(54*34)
+# first code represents sum of 2 values given
+# second code represents product of 2 values
+
+name = "shivam das"
+age = 21
+# the value which was written inside the strings are assigned by some name and number that represents the value of the given variable 
+
+print(name)
+print(age) 
+print(name,age)
+print("hey iam",name,"my age",age)
+# insted of printing name we can simply give the variable to print that given word or number
+# the print statement can be done in single line also
+
+name2 = name
+print(name,age,name2)
+print(name2)                                                                     
+# the above code represents the assignment rule 
+# that means storing the let side value to the right side value known as a=b
+
+# in the above code the identifiers rule also takes place where the numeric value or any special character like under score, upper case, lower case and any digit can be assigned after the name 
+
+
+name = "shivam"
+age = 21
+price = 4500.50
+
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(name2))
+# the above code represent the type of the data that were given inside the variable
+
+
+print(type(name),type(age),type(price))
+
+
+
+
+age = 21
+old = True
+a = None
+
+print(type(old))
+print(type(a))
+print(type(old),type(a))
+# the above code represents the boolean and none value inside the function type
+
+a = 3
+b = 4
+sum = (a+b)
+diff = (a-b)
+print(sum,diff)
+# the above code represents the sum and difference of two numbers
+
+
+a,b = 2,3
+txt = "@"
+print(a*txt*b)
+"""Rule:1 - The above code represents that the strings and the integers can be operate together by using the *
+(THIS COMES UNDER EXPRESSION EXECUTION RULE)"""
+
+a,b = "2",3
+txt = "@"
+print((a+txt)*b)
+
+a,b = "2",3
+txt = "@"
+print(a+txt*b)
+
+a,b = "2","3"
+txt = "@"
+print(a+txt+b)
+"""Rule:2 - The above code represents that the string and string can be operated by using the +  
+(THIS COMES UNDER EXPRESSION EXECUTION RULE)"""
+
+
+
+
+
+
+a = 2
+b = 3
+c = a/b
+print(c)
+"""Rule:5 - The above code represents the division between the 2 integers which gives the output as float 
+(THIS COMES UNDER EXPRESSION EXECUTION RULE)"""
+
+
+
+
+name = input("name : ")
+age = int(input("age :"))
+price = float(input("price :"))
+print("my name is",name,"my age is",age)
+"""the above code represents the INPUT RULE in the python 
+where word or any sentence can be written in input statement but the numbers or any other decimal value will be assign as their type"""
+
