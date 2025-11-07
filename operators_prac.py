49641898b7f8fd66d7edca483de137c623694d81
shivam <shivam984861@gmail.com>
2025-11-07 19:39:54 +0530

    new changes

--- operators_prac.py
+++ operators_prac.py
@@ -79,3 +79,31 @@ print(num)
 num = 33
 num %= 7
 print(num)
+
+
+# LOGICAL OPERATORS
+a = 30
+b = 20
+print(not False)
+print(not True)
+print(a >= b)
+# not operator gives the exact opposite of the boolean value
+
+a = True
+b = True
+print(a and b)
+a = True
+b = False
+print(a and b)
+# and operator returns True only if both the operands are True other wise it returns false if one condition is False
+
+a = True
+b = False
+print(a or b)
+a = False
+b = False
+print(a or b)
+a = True
+b = True
+print(a or b)
+# or operator returns True if at least one of the operands is True otherwise it returns False if both conditions are False
