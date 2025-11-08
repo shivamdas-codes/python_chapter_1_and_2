5af194bf3bfad394b17ad634ebc6983c76d3ec0d
shivam <shivam984861@gmail.com>
2025-11-08 22:39:19 +0530

    new changes

---
+++ all_practice_programs.py
@@ -0,0 +1,15 @@
+# write a program to input 2 numbers and print their sum, difference, product, quotient and remainder
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+quotient_result = number1 / number2
+remainder_result = number1 % number2
+
+print("Sum:", sum_result)
+print("Difference:", difference_result)
+print("Product:", product_result)
+print("Quotient:", quotient_result)
+print("Remainder:", remainder_result)
