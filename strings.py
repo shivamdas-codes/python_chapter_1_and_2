9b5b0ffd2fee587a312255f8ec03bd52f904c0ab
shivam <shivam984861@gmail.com>
2025-11-10 22:53:55 +0530

    new changes

---
+++ strings.py
@@ -0,0 +1,5 @@
+# the strings can be written in three ways
+str1 = 'hello world'          # using single quotes
+str2 = "hello world"          # using double quotes
+str3 = '''hello world'''        # using triple quotes
+
