d33323080e8052a300d090d8e6ee5bbc78516f91
shivam <shivam984861@gmail.com>
2025-11-11 22:41:48 +0530

    new changes

--- strings.py
+++ strings.py
@@ -1,13 +1,31 @@
-# # the strings can be written in three ways
-# str1 = 'hello world'          # using single quotes
-# str2 = "hello world"          # using double quotes
-# str3 = '''hello world'''        # using triple quotes
+# the strings can be written in three ways
+str1 = 'hello world'          # using single quotes
+str2 = "hello world"          # using double quotes
+str3 = '''hello world'''        # using triple quotes
 
 
+
+# escape sequence in strings
 str1 = "hey this is shivam das\ncurrently im pursuing my mca"
 # the above line was assigned by '\n' which is used to break the line in the output as two sentences
 str2 = "hey this is shivam das\tcurrently im pursuing my mca"
 # the above line was assigned by '\t' which is used to give a tab space in the output
-
 print(str1 )
 print(str2 )
+
+
+#1.concatenation of strings
+str1 = "hello "
+str2 = "world"
+final_string = str1 + str2
+print(final_string)
+
+
+#2.lenghth of the string
+str1 = "hello world"
+print(len(str1))
+str2 = "shivam das"
+print(len(str2))
+print(str1 + " " + str2)
+print(len(str1 + " " + str2))
+
