f0bd7e187db862aeac8b80501f6587bed7052de6
shivam <shivam984861@gmail.com>
2025-11-16 20:42:17 +0530

    new changes

--- hackerrank.py
+++ hackerrank.py
@@ -17,14 +17,28 @@
 
     
 
-"""The provided code stub reads two integers from STDIN, a and b. Add code to print three lines where:
-The first line contains the sum of the two numbers.
-The second line contains the difference of the two numbers (first - second).
-The third line contains the product of the two numbers."""
+# """The provided code stub reads two integers from STDIN, a and b. Add code to print three lines where:
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers."""
+
+# a = int(input())
+# b = int(input())
+# sum = (a + b)
+# diff = (a - b)
+# prod = (a * b)
+# print(sum,diff,prod)
+
+
+
+"""The provided code stub reads two integers, a and b, from STDIN.Add logic to print two lines. 
+The first line should contain the result of integer division, a // b. 
+The second line should contain the result of float division, a / b.
+No rounding or formatting is necessary."""
 
 a = int(input())
 b = int(input())
-sum = (a + b)
-diff = (a - b)
-prod = (a * b)
-print(sum,diff,prod)
+num1 = (a // b)
+num2 = (a / b)
+print(num1)
+print(num2)
