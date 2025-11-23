db63cc850250e44bf63cf6b84e7071c2cc6c87fb
shivam <shivam984861@gmail.com>
2025-11-23 18:44:44 +0530

    new changes

--- inputs.py
+++ inputs.py
@@ -1,3 +1,11 @@
+name = input("name : ")
+age = int(input("age :"))
+price = float(input("price :"))
+print("my name is",name,"my age is",age)
+"""the above code represents the INPUT RULE in the python 
+where word or any sentence can be written in input statement but the numbers or any other decimal value will be assign as their type"""
+
+
 input("name : ")
 # we can simply give anything as input in the input statement
 name = input("hey iam : ")
