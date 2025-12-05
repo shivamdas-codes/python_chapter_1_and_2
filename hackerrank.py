3f6896e61146a99cf6c5568227b5a21a55252e87
shivam <shivam984861@gmail.com>
2025-12-05 21:52:50 +0530

    initial changes

--- hackerrank.py
+++ hackerrank.py
@@ -18,7 +18,7 @@ elif n % 2 == 0 and n > 20:
 
     
 
-"""The provided code stub reads two integers from STDIN, a and b. Add code to print three lines where:
+"""(2).The provided code stub reads two integers from STDIN, a and b. Add code to print three lines where:
 The first line contains the sum of the two numbers.
 The second line contains the difference of the two numbers (first - second).
 The third line contains the product of the two numbers."""
@@ -32,7 +32,7 @@ print(sum,diff,prod)
 
 
 
-"""The provided code stub reads two integers, a and b, from STDIN.Add logic to print two lines. 
+"""(3).The provided code stub reads two integers, a and b, from STDIN.Add logic to print two lines. 
 The first line should contain the result of integer division, a // b. 
 The second line should contain the result of float division, a / b.
 No rounding or formatting is necessary."""
