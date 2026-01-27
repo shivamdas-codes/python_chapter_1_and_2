3d63898b8f3e169a2bbd75f955fe528351497fb5
shivam <shivam984861@gmail.com>
2026-01-27 22:49:18 +0530

    initial changes

--- all_practice_programs.py
+++ all_practice_programs.py
@@ -1,4 +1,3 @@
-
 #(2).write a program to input side of a square and print its area and perimeter
 side1 = float(input("Enter the square side1: "))
 side2 = int(input("Enter the square side2: "))
@@ -8,8 +7,6 @@ area = side1 * side1
 print("square side:", side2 ** 2)
 print("Area of square:", area)
 print("Perimeter of square:", 4 * side3,4 * side4)
-
-
 
 #(3).write a program to input 2 floating point numbers and print their average
 num1 = float(input("Enter first number: "))
@@ -21,14 +18,8 @@ the_average = (num1 + num2) / 2
 print("Average:", the_average)
 print("Average:", (num3 + num4) / 2)
 
-
-
 # (5).write a program to input users first name and print its length
 user_1 = input("first name :")
 firstname = len(user_1)
 print(firstname)
 print(len(user_1))   # prints the length of the string
-
-
-
-
